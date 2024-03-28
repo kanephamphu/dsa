e26035b31bb250db77bf85788ea4b2da54ae3476
Tai Pham Phu <taiphupham@hitachids.com>
2024-03-28 16:23:04 +0700

    Add tree

---
+++ data-structure/tree.ts
@@ -0,0 +1,18 @@
+class TreeNode {
+    private value: any;
+    private leftNode: TreeNode;
+    private rightNode: TreeNode;
+
+    constructor(value: any) {
+        this.value = value;
+    }
+    
+}
+
+class Tree {
+    root: TreeNode;
+
+    addNode(value) {
+
+    }
+}
